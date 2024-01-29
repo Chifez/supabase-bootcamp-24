9b9f3092b165643817b24821e06a27fb429453ff
Rotimi Best <best@classroomio.com>
2024-01-29 20:54:53 +0100

    chore: improve home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,18 @@
-<script>
-  import { supabase } from '$lib/supabase';
-  import { onMount } from 'svelte';
+<section
+  class="w-[90vw] h-[80vh] flex flex-col items-center justify-center my-10"
+>
+  <img
+    src="https://classroomio.com/logo-512-bg-white.png"
+    alt="ClassroomIO Logo"
+    class="w-32 h-32"
+  />
+  <h1 class="text-5xl">Welcome to SupaCamp</h1>
+  <p class="text-lg mb-10">The Supabase Bootcamp</p>
 
-  /**
-   * @type {any[]}
-   */
-  let countries = [];
-
-  onMount(async () => {
-    const { data } = await supabase
-      .from('countries')
-      .select()
-      .order('id', { ascending: true });
-
-    if (!data) return;
-
-    countries = data;
-  });
-</script>
-
-<ul>
-  {#each countries as country}
-    <li>{country.id}. {country.name} - {country.created_at}</li>
-  {/each}
-</ul>
+  <p class="text-2xl">
+    Brought to you by <a
+      class="text-blue-700 underline"
+      href="https://classroomio.com">ClassroomIO</a
+    >
+  </p>
+</section>
